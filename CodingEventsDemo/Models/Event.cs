edc5efa15cc5cf037d498dd683d08fe956cad04f
ozlemyalcin <omersam33@gmail.com>
2021-02-08 14:52:05 -0600

    20.3.4

--- CodingEventsDemo/Models/Event.cs
+++ CodingEventsDemo/Models/Event.cs
@@ -11,7 +11,10 @@ namespace CodingEventsDemo.Models
 
         public string ContactEmail { get; set; }
 
-        public EventType Type { get; set; }
+       //delete: public EventType Type { get; set; }
+
+        public int CategoryId { get; set; }
+        public EventCategory Category { get; set; }
 
         public int Id { get; set; }
 
@@ -20,6 +23,7 @@ namespace CodingEventsDemo.Models
             Name = name;
             Description = description;
             ContactEmail = contactEmail;
+         
         }
 
         public Event()
