2bcd721cf2325016c740443d5c50d9bc4b4f7f96
ozlemyalcin <omersam33@gmail.com>
2021-02-10 14:15:38 -0600

    20.4

---
+++ CodingEventsDemo/ViewModels/EventDetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CodingEventsDemo.Models;
+
+namespace CodingEventsDemo.ViewModels
+{
+    //ViewModel that will be the @model PageModel age  for a detail model
+    public class EventDetailViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ContactEmail { get; set; }
+        public string CategoryName { get; set; }
+
+        public EventDetailViewModel(Event theEvent)
+        {
+            Name = theEvent.Name;
+            Description = theEvent.Description;
+            ContactEmail = theEvent.ContactEmail;
+            CategoryName = theEvent.Category.Name;
+        }
+
+    }
+}
